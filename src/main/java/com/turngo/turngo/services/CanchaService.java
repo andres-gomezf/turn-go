2e76787f470ed6486720a2846c82c27291a5605a
Valentín <correavale2004@gmail.com>
2025-08-20 14:42:42 -0300

    agrego servicios

--- src/main/java/com/turngo/turngo/services/CanchaService.java
+++ src/main/java/com/turngo/turngo/services/CanchaService.java
@@ -1,4 +1,33 @@
 package com.turngo.turngo.services;
 
+import com.turngo.turngo.entities.Cancha;
+import com.turngo.turngo.entities.Cliente;
+import com.turngo.turngo.repositories.CanchaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CanchaService {
+
+    @Autowired
+    private CanchaRepository canchaRepository;
+
+    public List<Cancha> findAll() {
+        return canchaRepository.findAll();
+    }
+
+    public Optional<Cancha> findById(Long id) {
+        return canchaRepository.findById(id);
+    }
+
+    public Cancha save(Cancha cancha) {
+        return canchaRepository.save(cancha);
+    }
+
+    public void delete(Long id) {
+        canchaRepository.deleteById(id);
+    }
 }
--- src/main/java/com/turngo/turngo/services/ClienteService.java
+++ src/main/java/com/turngo/turngo/services/ClienteService.java
@@ -1,4 +1,33 @@
 package com.turngo.turngo.services;
 
+import com.turngo.turngo.entities.Cliente;
+import com.turngo.turngo.entities.Turno;
+import com.turngo.turngo.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> findById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void delete(Long id) {
+        clienteRepository.deleteById(id);
+    }
 }
