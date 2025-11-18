e629b1674b0530dd1b6299a51eab088548c02173
correavalentin <correavale2004@gmail.com>
2025-11-18 18:24:56 -0300

    El envio de mail ahora es asincrono

--- src/main/java/com/turngo/turngo/TurngoApplication.java
+++ src/main/java/com/turngo/turngo/TurngoApplication.java
@@ -2,8 +2,10 @@ package com.turngo.turngo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TurngoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/turngo/turngo/notifications/EmailNotification.java
+++ src/main/java/com/turngo/turngo/notifications/EmailNotification.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 
@@ -20,6 +21,7 @@ public class EmailNotification {
     @Value("${turngo.mail.from:no-reply@turngo.local}")
     private String fromAddress;
 
+    @Async
     public void sendNotification(Turno t) {
 
             Cliente cliente = t.getCliente();
